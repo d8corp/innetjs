90d6188d12cfb8f175b8082a367ce1eeca96e9f6
Mike Lysikov <d8corp@mail.ru>
2022-11-13 14:51:32 +0300

    v2.2.23

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 ## v2.2
 
+### v2.2.23 [![13.11.2022](https://img.shields.io/date/1668340263)](https://github.com/d8corp/innetjs/tree/v2.2.23)
+- fix env bugs
+
 ### v2.2.22 [![13.11.2022](https://img.shields.io/date/1668339846)](https://github.com/d8corp/innetjs/tree/v2.2.22)
 - fix env bugs
 
--- release/index.mjs
+++ release/index.mjs
@@ -38,7 +38,7 @@ import { reporter, convertIndexFile, getFile } from './helpers.mjs';
 import { updateDotenv } from './updateDotenv.mjs';
 
 (function () {
-  const env = {"__INNETJS__PACKAGE_VERSION":"2.2.22"};
+  const env = {"__INNETJS__PACKAGE_VERSION":"2.2.23"};
   if (typeof process === 'undefined') {
     globalThis.process = { env: env };
   } else if (process.env) {
