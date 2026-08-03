b7cef138e89fb5d5f6d192762bdcc30c6b02255f
Deight <d8corp@mail.ru>
2026-08-03 14:28:39 +0300

    refactor: split InnetJS class into separate modules

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface InnetJSParams {
+  envPrefix?: string
+  projectFolder?: string
+  baseUrl?: string
+  publicFolder?: string
+  releaseFolder?: string
+  buildFolder?: string
+  srcFolder?: string
+  sourcemap?: boolean
+  cssModules?: boolean
+  cssInJs?: boolean
+  sslKey?: string
+  sslCrt?: string
+  proxy?: string
+  simulateIP?: string
+  port?: number
+  api?: string
+  tsconfig?: string
+}
+
+export interface ReleaseOptions {
+  node?: boolean
+  index?: string
+  pub?: boolean
+  min?: boolean
+}
